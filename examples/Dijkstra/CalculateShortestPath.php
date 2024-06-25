c95a4afb7062b6d6b9b1e8d9607258372ade4095
Matt Browne <mbrowne@artnet.com>
2024-06-25 09:28:54 -0400

    Revert "fix naming"
    This reverts commit 69345942c05831023e858563980832f7aafc1991.

--- examples/Dijkstra/CalculateShortestPath.php
+++ examples/Dijkstra/CalculateShortestPath.php
@@ -42,8 +42,8 @@ namespace UseCases
                 $this->tentativeDistances->setDistanceTo($n, INF);
             }
 
-            while ($closestToDestination = $this->processCurrentNode($this->destinationNode)) {
-                $this->currentNode = $closestToDestination->addRole('CurrentNode', $this);
+            while ($closestFromStart = $this->processCurrentNode($this->destinationNode)) {
+                $this->currentNode = $closestFromStart->addRole('CurrentNode', $this);
             }
 
             for (
@@ -62,7 +62,7 @@ namespace UseCases
             if ( !$this->unvisitedNodes->hasNode($destinationNode) ) {
                 return null;
             }
-            return $this->unvisitedNodes->findClosestToDestination();
+            return $this->unvisitedNodes->findClosestFromStart();
         }
     }
 }
@@ -119,14 +119,14 @@ namespace UseCases\CalculateShortestPath\Roles
 
     trait Neighbor
     {
-        // Is there a shorter path (from this node to the destination) than previously
+        // Is there a shorter path (from the start node to this node) than previously
         // determined?
         function shorterPathAvailable() {
             $tentativeDistances = $this->context->tentativeDistances;
             $currentNode = $this->context->currentNode;
 
-            $distanceFromCurrentToDestination = $tentativeDistances->distanceTo($currentNode);
-            $netDistance = $distanceFromCurrentToDestination + $currentNode->distanceTo($this->self);
+            $distanceFromStartToCurrent = $tentativeDistances->distanceTo($currentNode);
+            $netDistance = $distanceFromStartToCurrent + $currentNode->distanceTo($this->self);
 
             if ($netDistance < $tentativeDistances->distanceTo($this->self)) {
                 $tentativeDistances->setDistanceTo($this->self, $netDistance);
@@ -150,12 +150,9 @@ namespace UseCases\CalculateShortestPath\Roles
             return $this->count() === 0;
         }
 
-        // QUESTION: Is this actually the closest from the START?
-        // Need to double-check
-        //
         // possible refactoring:
-        // This could be DestinationNode.findClosestUnvisitedNode()
-        function findClosestToDestination() {
+        // This could be StartNode.findClosestUnvisitedNode()
+        function findClosestFromStart() {
             $this->context->currentNode->determinePreviousInPath();
 
             $tentativeDistances = $this->context->tentativeDistances;
